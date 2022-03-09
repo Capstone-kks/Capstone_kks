f3fd12aaa51dfa1bc601015a00b2daa0ffe7e159
YouKyeong Kang <jinny3685@naver.com>
2022-03-09 10:54:13 +0900

    Initial commit

---
+++ src/test/java/com/kks/demo/KksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kks.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
