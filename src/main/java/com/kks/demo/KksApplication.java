29bcccac99d156bb16d5bfdde696cbe556a7679a
YouKyeong Kang <jinny3685@naver.com>
2022-03-09 22:37:44 +0900

    initial commit

---
+++ src/main/java/com/kks/demo/KksApplication.java
@@ -0,0 +1,13 @@
+package com.kks.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KksApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KksApplication.class, args);
+	}
+
+	//http://localhost:8080/로 접속
+}
