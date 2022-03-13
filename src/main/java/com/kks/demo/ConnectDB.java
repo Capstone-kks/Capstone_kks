0d43bde515306f02ccee208d2fb9488b86b7bc03
YouKyeong Kang <jinny3685@naver.com>
2022-03-14 00:47:41 +0900

    application.properties - mysql 작성

--- src/main/java/com/kks/demo/ConnectDB.java
+++ src/main/java/com/kks/demo/ConnectDB.java
@@ -1,9 +1,15 @@
 package com.kks.demo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+@Slf4j
+@PropertySource("classpath:application.properties")
 public class ConnectDB {
     public void main(String[] args){
         String url = "jdbc:mysql://database-1.cltih7jbgequ.ap-northeast-2.rds.amazonaws.com/mydb?autoReconnect=true&useSSL=false";
